a4e04d12aae5f8821ff0de100a92222867d79511
CCDM19 <joseinzunzam2@gmail.com>
2018-05-24 13:06:12 -0600

    Buscar usuario

--- servidor/js/eventos.js
+++ servidor/js/eventos.js
@@ -14,7 +14,12 @@ var inicioApp = function(){
             data: parametros,
             success: function(response){
                 if(response.respuesta == true){
-                    alert("Bienvenido");
+                    //ocultamos el inicio
+                    $("#secInicio").hide("slow");
+                    //Aparecemos usuarios
+                    $("#frmUsuarios").show("slow");
+                    //cursor en el primer cuadro de texto
+                    $("#txtNombreUsuario").focus();
                 }else{
                     alert("Usuario o Clave incorrecta");
                 }
@@ -26,7 +31,42 @@ var inicioApp = function(){
             }
         });
     }
+    var buscarUsuario = function(){
+    	var usuario = $("#txtNombreUsuario").val();
+    	var parametros =$"opc=buscarUsuario"+
+    					 "&usuario="+usuario+
+    					 "&aleatorio="+Math.random();
+    	if(usuario !=""){
+    		$.ajax({
+            cache:false,
+            type: "POST",
+            dataType: "json",
+            url: "php/buscarusuario.php",
+            data: parametros,
+            success: function(response){
+            	if(reponse.respuesta ==true){
+            		$("#txtNombre").val(response.nombre);
+            		$("#txtClaveUsuario").val(response.clave);
+            	}else{
+            		$("#txtNombre").focus();
+            	}
+
+            },
+            error: function(xhr,ajaxOptions,throwError){
+                console.log(xhr);
+               
+            }
+        });
+    	}
+    }
+    var teclaNombreUsuario = function(tecla){
+    	if(tecla.which == 13){//enter
+    		buscarUsuario();
+
+    	}
+    }
     $("#btnAceptar").on("click",Aceptar);
+    $("#txtNombreUsuario").on("keypress",teclaNombreUsuario);
 }
 
 $(document).ready(inicioApp);
